ec929680a7a4ff0c5a88170eef1be2f76d003299
vlad <vladislav.96@mail.ru>
2022-05-04 19:25:54 +0300

    Fixed IndexControl

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -3,14 +3,19 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accident.repository.AccidentMem;
+import ru.job4j.accident.service.AccidentService;
 
 @Controller
 public class IndexControl {
+    private AccidentService accidentService;
+
+    public IndexControl(AccidentService accidentService) {
+        this.accidentService = accidentService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        AccidentMem accidentMem = new AccidentMem();
-        model.addAttribute("accidents", accidentMem.getAll().values());
+        model.addAttribute("accidents", accidentService.getAll());
         return "index";
     }
 }
--- src/main/java/ru/job4j/accident/service/AccidentService.java
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -1,7 +1,35 @@
 package ru.job4j.accident.service;
 
 import org.springframework.stereotype.Service;
+import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.repository.AccidentMem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccidentService {
+
+    private AccidentMem accidentMem;
+
+    public AccidentService(AccidentMem accidentMem) {
+        this.accidentMem = accidentMem;
+    }
+
+    public void add(Accident accident) {
+        accidentMem.add(accident);
+    }
+
+    public Accident deleteById(int elId) {
+        return accidentMem.deleteById(elId);
+    }
+
+    public Accident getById(int id) {
+        return accidentMem.getById(id);
+    }
+
+    public List<Accident> getAll() {
+        return new ArrayList<>(accidentMem.getAll().values());
+    }
+
 }
